1cde48094352bfa8c42b0c2a52b86731d8bb111e
PracovnyBod <cloudiadaq@gmail.com>
2021-02-15 16:51:47 +0100

    cv1 update

---
+++ PY/misc/figsc_ar02_fig02.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+
+from figFcns_TeX import *
+
+
+
+figPlotParam = fcnDefaultFigSize(12, 0.15, 0.95, 0.06, 0.5, 13)
+fig = plt.figure(figNameNum, figsize=figPlotParam[0:2])
+
+subPlots = gridspec.GridSpec(3, 1, )
+
+
+#------------------
+ax0 = plt.subplot(subPlots[0])
+
+ax0.set_title(u'Výstup', x=0.01, y=1.02, ha='left')
+
+ax0.plot(t_log_rec, x_log_rec[0][:,0],
+         '-', lw=0.75, color='k',
+         label = u'$a={}$'.format(testPripady['pripad1'])
+         )
+
+ax0.plot(t_log_rec, x_log_rec[1][:,0],
+         '-', lw=0.75, color='k', dashes=[5,2],
+         label = u'$a={}$'.format(testPripady['pripad2'])
+         )
+
+ax0.plot(t_log_rec, x_log_rec[2][:,0],
+         '-', lw=0.75, color='k', dashes=[2,2],
+         label = u'$a={}$'.format(testPripady['pripad3'])
+         )
+
+#------------------
+ax1 = plt.subplot(subPlots[1])
+
+ax1.set_title(u'Akčný zásah', x=0.01, y=1.02, ha='left')
+
+ax1.plot(t_log_rec, -x_log_rec[0][:,1]*x_log_rec[0][:,0],
+         '-', lw=0.75, color='k',
+         )
+
+ax1.plot(t_log_rec, -x_log_rec[1][:,1]*x_log_rec[1][:,0],
+         '-', lw=0.75, color='k', dashes=[5,2],
+         )
+
+ax1.plot(t_log_rec, -x_log_rec[2][:,1]*x_log_rec[2][:,0],
+         '-', lw=0.75, color='k', dashes=[2,2],
+         )
+
+#------------------
+ax2 = plt.subplot(subPlots[2])
+
+ax2.set_title(u'Adaptovaný parameter', x=0.01, y=1.02, ha='left')
+
+ax2.plot(t_log_rec, x_log_rec[0][:,1],
+         '-', lw=0.75, color='k',
+         )
+
+ax2.plot(t_log_rec, x_log_rec[1][:,1],
+         '-', lw=0.75, color='k', dashes=[5,2],
+         )
+
+ax2.plot(t_log_rec, x_log_rec[2][:,1],
+         '-', lw=0.75, color='k', dashes=[2,2],
+         )
+
+#------------------
+for ax in fig.get_axes():
+    ax.set_xlabel(u'čas', ha='left', va='bottom')
+
+#------------------
+fcnDefaultLayoutAdj(fig, figPlotParam[2], figPlotParam[3], figPlotParam[4], figPlotParam[5])
+
+for ax in fig.get_axes():
+
+    if ax in [ax0, ax1, ax2]:
+        fcnDefaultAxisStyle(ax)
+
+        ax.xaxis.set_minor_locator(AutoMinorLocator())
+        ax.yaxis.set_minor_locator(AutoMinorLocator())
+
+
+    if ax in [ax0, ax1, ax2]:
+        ax.xaxis.set_label_coords(1.04, -0.2)
+
+
+    #-----------------------------
+
+    handles_ax, labels_ax = ax.get_legend_handles_labels()
+
+    if ax in [ax0, ax1, ax2]:
+        ax.legend(handles_ax, labels_ax, ncol=1, handlelength=1.2, loc=2, bbox_to_anchor=(1.04, 1.0))
+
+
+
+
+#------------------
+
+plt.savefig(figSaveDir + figName + '_' + '{}'.format(figNameNum) +'.pdf')
